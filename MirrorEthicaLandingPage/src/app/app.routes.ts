9977515dcb3d01a812e9c77ec7c8deb863930227
icarus-2 <ebriffett01@gmail.com>
2025-05-11 14:10:41 -0400

    LEERS Page done

--- MirrorEthicaLandingPage/src/app/app.routes.ts
+++ MirrorEthicaLandingPage/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { LeersComponent } from './leers/leers.component';
 import { ContactComponent } from './contact/contact.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { BlogComponent } from './blog/blog.component';
 
 export const routes: Routes = 
 [
@@ -11,5 +12,6 @@ export const routes: Routes =
     {path: "about", component: AboutComponent},
     {path: "leers", component: LeersComponent},
     {path: "contact", component: ContactComponent},
+    {path: "blog", component: BlogComponent},
     {path: "**", component: NotFoundComponent}
 ];
