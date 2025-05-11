6aab315658a08004522aa5955b2f2f9c8738336b
icarus-2 <ebriffett01@gmail.com>
2025-05-11 16:30:17 -0400

    Started the about carousel

--- MirrorEthicaLandingPage/src/app/about/about.component.ts
+++ MirrorEthicaLandingPage/src/app/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-about',
-  imports: [RouterLink],
+  imports: [RouterLink, NgbCarousel, NgbAlert],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
