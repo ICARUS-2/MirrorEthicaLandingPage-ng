c3021bf01105ade21b839723b47fbbb6265519f0
icarus-2 <ebriffett01@gmail.com>
2025-05-11 13:12:39 -0400

    Homepage background image, leers image and placeholder text

--- MirrorEthicaLandingPage/src/app/homepage/homepage.component.ts
+++ MirrorEthicaLandingPage/src/app/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
