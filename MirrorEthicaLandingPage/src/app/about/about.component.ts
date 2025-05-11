ef6993f01abc9480a4bb7198f3853e85aeef6d86
icarus-2 <ebriffett01@gmail.com>
2025-05-11 17:03:02 -0400

    About carousel done

--- MirrorEthicaLandingPage/src/app/about/about.component.ts
+++ MirrorEthicaLandingPage/src/app/about/about.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { FlipCardComponent } from "../flip-card/flip-card.component";
 
 @Component({
   selector: 'app-about',
-  imports: [RouterLink, NgbCarousel, NgbAlert],
+  imports: [RouterLink, NgbCarousel, FlipCardComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
